6869871786ee9c06221814dc50a3306a3fc59bea
DanaNB02 <dbaeisa25@twq.idserve.net>
2025-09-29 11:53:05 +0300

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Dana on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
